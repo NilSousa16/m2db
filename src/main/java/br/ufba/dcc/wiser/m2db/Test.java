3ae6a6e52facbf657490ad53910b83d394877e55
NilSousa16 <nil.sousa16@gmail.com>
2023-01-25 18:26:18 -0300

    Improved data generation

--- src/main/java/br/ufba/dcc/wiser/m2db/Test.java
+++ src/main/java/br/ufba/dcc/wiser/m2db/Test.java
@@ -2,6 +2,8 @@ package br.ufba.dcc.wiser.m2db;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Test {
@@ -19,9 +21,25 @@ public class Test {
 //		double valor = new Random().nextDouble(0, 1);
 //		System.out.println("Valor: " + valor);
 		
-		double roundOff = Math.round(new Random().nextDouble(0, 1) * 100.0) / 100.0;
+//		double roundOff = Math.round(new Random().nextDouble(0, 1) * 100.0) / 100.0;
+//		
+//		System.out.println("Novo valor: " + roundOff);
 		
-		System.out.println("Novo valor: " + roundOff);
+//		List<String> gatewayIdList = new ArrayList<String>();
+//		
+//		gatewayIdList.add("Nilson");
+//		gatewayIdList.add("Samuel");
+//		gatewayIdList.add("Djor");
+//		
+//		System.out.println("Tamanho: " + gatewayIdList.size());
+//		
+//		System.out.println("VAlor: " + gatewayIdList.get(2));
+		
+		String typeDeviceList[] = { "temperature", "humidity", "fire", "luminosity", "ultrasonic", "gas", "movement",
+		"atmospheric" };
+		
+		System.out.println(">>> " + typeDeviceList.length);
+		
 	}
 
 }
---
+++ src/main/java/br/ufba/dcc/wiser/m2db/command/DataGenerator.java
@@ -0,0 +1,156 @@
+package br.ufba.dcc.wiser.m2db.command;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import br.ufba.dcc.wiser.m2db.service.DeviceServiceDB;
+import br.ufba.dcc.wiser.m2db.service.DeviceStatusServiceDB;
+import br.ufba.dcc.wiser.m2db.service.GatewayServiceDB;
+import br.ufba.dcc.wiser.m2db.service.GatewayStatusServiceDB;
+import br.ufba.dcc.wiser.m2model.model.Device;
+import br.ufba.dcc.wiser.m2model.model.DeviceStatus;
+import br.ufba.dcc.wiser.m2model.model.Gateway;
+import br.ufba.dcc.wiser.m2model.model.GatewayStatus;
+
+@Component(service = DataGenerator.class, property = { "osgi.command.scope=fot-dev",
+		"osgi.command.function=dataGenerator" })
+public class DataGenerator {
+
+	@Reference
+	private GatewayServiceDB gatewayServiceDB;
+
+	@Reference
+	private DeviceServiceDB deviceServiceDB;
+
+	@Reference
+	private GatewayStatusServiceDB gatewayStatusServiceDB;
+
+	@Reference
+	private DeviceStatusServiceDB deviceStatusServiceDB;
+
+	SimpleDateFormat form = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+	public void dataGenerator() throws InterruptedException {
+
+		Random random = new Random();
+		Device device = new Device();
+		String idDevice;
+
+		List<String> deviceIdList = new ArrayList<String>();
+		List<String> gatewayIdList = new ArrayList<String>();
+
+		/*
+		 * Configuration for data generation
+		 */
+		final int TOTAL_GATEWAY = 50;
+		final int TOTAL_GATEWAY_STATUS = 50;
+		final int TOTAL_DEVICE = 500;
+		final int TOTAL_DEVICE_STATUS = 100;
+
+		/**
+		 * Gateways
+		 */
+
+		Gateway gateway = new Gateway();
+
+		System.out.println(">>> Inserting data about the gateways");
+
+		String smartSolutionList[] = { "smart traffic", "smart parking", "structural health", "water quality",
+				"traffic congestion", "smart lighting", "air pollution", "forest fire detection" };
+
+		for (int i = 0; i < TOTAL_GATEWAY; i++) {
+			String mac = Integer.toString(random.nextInt(32)) + ":" + Integer.toString(random.nextInt(32)) + ":"
+					+ Integer.toString(random.nextInt(32)) + ":" + Integer.toString(random.nextInt(32)) + ":"
+					+ Integer.toString(random.nextInt(32)) + ":" + Integer.toString(random.nextInt(32));
+
+			String ip = Integer.toString(random.nextInt(192)) + "." + Integer.toString(random.nextInt(192)) + "."
+					+ Integer.toString(random.nextInt(192)) + "." + Integer.toString(random.nextInt(192));
+
+			gatewayIdList.add(mac);
+
+			gateway.setMac(mac);
+			gateway.setIp(ip);
+			gateway.setManufacturer("WiserUFBA");
+			gateway.setHostName("GT" + random.nextInt(192));
+			gateway.setStatus(true);
+			gateway.setDate(Calendar.getInstance());
+			gateway.setSolution(smartSolutionList[random.nextInt(8)]);
+			gateway.setCoordinates("57.0030, 86.2199");
+			gatewayServiceDB.add(gateway);
+		}
+
+		/**
+		 * Devices
+		 */
+
+		System.out.println(">>> Inserting data about the devices");
+
+		String typeDeviceList[] = { "temperature", "humidity", "fire", "luminosity", "ultrasonic", "gas", "movement",
+				"atmospheric" };
+		String categoryDeviceList[] = { "sensor", "actuador" };
+
+		for (int i = 0; i < TOTAL_DEVICE; i++) {
+
+			idDevice = Integer.toString(random.nextInt(10000));
+
+			deviceIdList.add(idDevice);
+
+			device.setId(idDevice);
+			device.setLocation("122.9689, -44.7436");
+			device.setDescription("DTM");
+			device.setTypeDevice(typeDeviceList[random.nextInt(typeDeviceList.length)]);
+			device.setCategory(categoryDeviceList[random.nextInt(categoryDeviceList.length)]);
+			device.setStatus(true);
+			device.setDate(Calendar.getInstance());
+
+			gateway = new Gateway();
+			gateway.setMac(gatewayIdList.get(random.nextInt(gatewayIdList.size())));
+			device.setGateway(gateway);
+			deviceServiceDB.add(device);
+		}
+
+		/**
+		 * Gateways Status
+		 */
+
+		System.out.println(">>> Inserting data about the gateways status");
+
+		for (String idGatewayList : gatewayIdList) {
+			for (int i = 0; i <= TOTAL_GATEWAY_STATUS; i++) {
+				GatewayStatus gatewayStatus = new GatewayStatus(idGatewayList, random.nextInt(100), random.nextInt(100),
+						random.nextInt(100), Calendar.getInstance());
+				Thread.sleep(10);
+
+				gatewayStatusServiceDB.add(gatewayStatus);
+			}
+		}
+
+		/**
+		 * Device Status
+		 */
+
+		System.out.println(">>> Inserting data about the device status");
+
+		String situationList[] = { "operational", "testing", "unused" };
+
+		for (String idDeviceList : deviceIdList) {
+			DeviceStatus deviceStatus;
+
+			for (int i = 0; i < TOTAL_DEVICE_STATUS; i++) {
+				deviceStatus = new DeviceStatus(idDeviceList, situationList[random.nextInt(situationList.length)],
+						Calendar.getInstance());
+				Thread.sleep(20);
+				deviceStatusServiceDB.add(deviceStatus);
+			}
+
+		}
+
+	}
+
+}
